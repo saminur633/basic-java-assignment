34b94de15d1a01ab683b7e3ed971956d3e462b01
saminur633 <rahmansaminur51@gmail.com>
2024-10-25 20:09:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function turnOn() {
+    document.getElementById("lightBulb").style.backgroundColor = "red";
+  }
+  
+  function turnOff() {
+    document.getElementById("lightBulb").style.backgroundColor = "#333";
+  }
+  
